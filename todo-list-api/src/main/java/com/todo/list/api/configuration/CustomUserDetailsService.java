34c56b11f6367fc552e91e32c62a68176d810681
Anderson <jabetancourtb@hotmail.com>
2024-08-20 20:13:26 -0500

    CustomUserDetailsService.java is modified

--- todo-list-api/src/main/java/com/todo/list/api/configuration/CustomUserDetailsService.java
+++ todo-list-api/src/main/java/com/todo/list/api/configuration/CustomUserDetailsService.java
@@ -2,7 +2,10 @@ package com.todo.list.api.configuration;
 
 import java.util.ArrayList;
 
+import com.todo.list.api.application.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,7 +17,7 @@ import com.todo.list.api.domain.model.user.UserRepository;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-	private UserRepository userRepository;
+	private final UserRepository userRepository;
 	
 	@Autowired 
 	public CustomUserDetailsService(UserRepository userRepository) {
@@ -23,11 +26,22 @@ public class CustomUserDetailsService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String email) {
-		User user = userRepository.getByEmail(email);
-		if(user == null) { 
-			throw new UsernameNotFoundException("Email " + email + " Not Found");
+		try {
+			User user = userRepository.getByEmail(email);
+
+			if(user == null) {
+				throw new UsernameNotFoundException("Email " + email + " Not Found");
+			}
+
+			if(!user.isEnabled()) {
+				throw new BadCredentialsException("User " + user.getFullName() + " is not enabled");
+			}
+			return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getEncryptedPassword(), new ArrayList<>());
 		}
-		
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getEncryptedPassword(), new ArrayList<>());
+		catch(EmptyResultDataAccessException exception) {
+			throw new UserNotFoundException();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
