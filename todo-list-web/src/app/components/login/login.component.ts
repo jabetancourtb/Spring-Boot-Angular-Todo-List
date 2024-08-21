0165f2ffbaeeefa56ad4e8a9374e439ed011b33e
Anderson <jabetancourtb@hotmail.com>
2024-08-20 20:21:38 -0500

    Login components are modified

--- todo-list-web/src/app/components/login/login.component.ts
+++ todo-list-web/src/app/components/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { ToastrService } from 'ngx-toastr';
 import { UserModel } from 'src/app/models/user/user.model';
-import { UserService } from 'src/app/services/user/user.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { JwtParseService } from 'src/app/services/util/jwtParse.service';
+import { TokenService } from 'src/app/services/util/token.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -21,9 +24,9 @@ export class LoginComponent implements OnInit {
     password : new FormControl(null, Validators.required)
   });
 
-  constructor(private userService: UserService
+  constructor(private authService: AuthService
+    , private tokenService : TokenService
     , private toastr: ToastrService
-    , private route: ActivatedRoute
     , private router: Router
     , private jwtParseService: JwtParseService) { }
 
@@ -32,20 +35,26 @@ export class LoginComponent implements OnInit {
 
   public login(): void {
 
-    let promise = this.userService.login(this.loginForm.get('email').value, this.loginForm.get('password').value);
+    let promise = this.authService.login(this.loginForm.get('email').value, this.loginForm.get('password').value);
 
     promise.then(data => {
-      this.jwtData = this.jwtParseService.parseJWT();
-      this.toastr.success('Welcome ' + this.jwtData['fullname']);
-      this.router.navigate(['tasks']);
+      this.tokenService.setToken(data.jwt);
+
+      let token = this.tokenService.getToken()
+
+      if(token != null && token != "") {
+        this.jwtData = this.jwtParseService.parseJWT();
+        this.toastr.success('Welcome ' + this.jwtData['fullname']);
+        this.router.navigate(['tasks']);
+      }
     });
 
     promise.catch(error => {
-      this.toastr.error('The credentials are wrong');
+      this.toastr.error(error.error.message);
     })
   }
 
   public goToSignUp(): void {
-      this.router.navigateByUrl('/sign-up');
+    this.router.navigateByUrl('/sign-up');
   }
 }
