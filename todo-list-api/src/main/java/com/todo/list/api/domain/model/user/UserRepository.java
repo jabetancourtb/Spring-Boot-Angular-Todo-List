b42daff8da556c77492ea13c3769ea73388c96b9
Anderson <jabetancourtb@hotmail.com>
2024-08-20 20:15:30 -0500

    User layers are modified

--- todo-list-api/src/main/java/com/todo/list/api/domain/model/user/UserRepository.java
+++ todo-list-api/src/main/java/com/todo/list/api/domain/model/user/UserRepository.java
@@ -3,6 +3,19 @@ package com.todo.list.api.domain.model.user;
 public interface UserRepository {
 
 	User create(final User user);
+
+	User getById(final long userId);
 	
-	User getByEmail(final String email);
+	User getByEmail(final String email) throws Exception;
+
+	User update(final User user) throws Exception;
+
+	boolean updatePassword(long userId, String encryptedNewPassword) throws Exception;
+
+	User getByVerificationCode(String verificationCode) throws Exception;
+
+	boolean updateVerificationCode(long userId, String verificationCode) throws Exception;
+
+	boolean updateState(long userId, boolean state) throws Exception;
+
 }
--- todo-list-api/src/main/java/com/todo/list/api/infrastructure/controller/user/UserController.java
+++ todo-list-api/src/main/java/com/todo/list/api/infrastructure/controller/user/UserController.java
@@ -2,53 +2,142 @@ package com.todo.list.api.infrastructure.controller.user;
 
 import java.util.NoSuchElementException;
 
+import com.todo.list.api.application.command.registeruser.RegisterUser;
+import com.todo.list.api.application.exception.PasswordNotMatchException;
+import com.todo.list.api.application.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.todo.list.api.domain.model.user.User;
-import com.todo.list.api.infrastructure.applicationservice.user.UserApplicationService;
-import com.todo.list.api.infrastructure.dto.jwt.JwtDTO;
-import com.todo.list.api.infrastructure.dto.user.UserDTO;
+import com.todo.list.api.application.applicationservice.user.UserApplicationService;
+import com.todo.list.api.application.dto.jwt.JwtDTO;
+import com.todo.list.api.application.dto.user.UserDTO;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	
-	private final UserApplicationService userService;
+	private UserApplicationService userService;
+
+	private RegisterUser registerUser;
 
 	@Autowired
-	public UserController(final UserApplicationService userService) {
+	public void setUserService(final UserApplicationService userService) {
 		this.userService = userService;
 	}
+
+	@Autowired
+	public void setRegisterUser(RegisterUser registerUser) {
+		this.registerUser = registerUser;
+	}
 	
-	@PostMapping
-	public ResponseEntity<User> signUp(@RequestBody final UserDTO userDto) {
+	@PostMapping("/sign-up")
+	public ResponseEntity<?> signUp(@RequestBody final UserDTO userDto) {
 		User user = null;
 		try {
-			user = userService.create(userDto);
+			user = registerUser.register(userDto);
 			return new ResponseEntity<>(user, HttpStatus.OK); 
 		} 
-		catch(NoSuchElementException e) {
-			return new ResponseEntity<>(user, HttpStatus.NOT_FOUND); 
+		catch(NoSuchElementException noSuchElementException) {
+			return new ResponseEntity<>(noSuchElementException, HttpStatus.NOT_FOUND);
+		}
+		catch(Exception exception) {
+			return new ResponseEntity<>(exception, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
-	@GetMapping("/{email}/{password}") 
-	public ResponseEntity<JwtDTO> login(@PathVariable("email") final String email, @PathVariable("password") final String password) throws Exception {
+	@GetMapping("/{email}/{password}")
+	public ResponseEntity<?> login(@PathVariable("email") final String email, @PathVariable("password") final String password) throws Exception {
 		String token = null;
 		try {
 			token = userService.login(email, password);
 			return new ResponseEntity<>(new JwtDTO(token), HttpStatus.OK); 
-		} 
-		catch(NoSuchElementException e) {
-			return new ResponseEntity<>(new JwtDTO(token), HttpStatus.UNAUTHORIZED); 
-		}		
+		}
+		catch(UserNotFoundException userNotFoundException) {
+			return new ResponseEntity<>(userNotFoundException, HttpStatus.BAD_REQUEST);
+		}
+		catch(NoSuchElementException noSuchElementException) {
+			return new ResponseEntity<>(noSuchElementException, HttpStatus.NO_CONTENT);
+		}
+		catch(Exception exception) {
+			return new ResponseEntity<>(exception, HttpStatus.UNAUTHORIZED);
+		}
 	}
+
+	@PutMapping
+	public ResponseEntity<JwtDTO> update(@RequestBody final UserDTO userDto) {
+		String token = null;
+		try {
+			token = userService.update(userDto);
+			return new ResponseEntity<>(new JwtDTO(token), HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(new JwtDTO(token), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/update-password/{userId}/{currentPassword}/{newPassword}")
+	public ResponseEntity<?> updatePassword(@PathVariable("userId") final long userId
+			, @PathVariable("currentPassword") final String currentPassword
+			, @PathVariable("newPassword") final String newPassword) {
+
+		try {
+			Boolean passwordUpdated = (Boolean) userService.updatePassword(userId, currentPassword, newPassword);
+			return new ResponseEntity<>(passwordUpdated, HttpStatus.OK);
+		}
+		catch(PasswordNotMatchException passwordNotMatchException) {
+			return new ResponseEntity<>("The password entered is not correct", HttpStatus.BAD_REQUEST);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/get-user-by-verification-code/{verificationCode}")
+	public ResponseEntity<?> getUserByVerificationCode(@PathVariable("verificationCode") final String verificationCode) {
+		try {
+			Object user = userService.getUserByVerificationCode(verificationCode);
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/get-user-by-email/{email}")
+	public ResponseEntity<?> getUserByEmail(@PathVariable("email") final String email) {
+		try {
+			Object user = userService.getUserByEmail(email);
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/update-verification-code/{userId}")
+	public ResponseEntity<?> updateVerificationCode(@PathVariable("userId") final long userId) {
+		try {
+			Boolean verificationCodeUpdated = userService.updateVerificarionCode(userId);
+			return new ResponseEntity<>(verificationCodeUpdated, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/update-state/{userId}/{state}")
+	public ResponseEntity<?> updateState(@PathVariable("userId") final long userId
+			, @PathVariable("state") final boolean state) {
+		try {
+			Boolean stateUpdated = userService.updateState(userId, state);
+			return new ResponseEntity<>(stateUpdated, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
