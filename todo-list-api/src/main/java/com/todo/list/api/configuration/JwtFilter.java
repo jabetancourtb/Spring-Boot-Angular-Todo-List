dc72a4aefbd28e17ffb10245fb826a3b62ee64dc
Anderson Betancourt <jabetancourtb@hotmail.com>
2024-08-20 20:35:23 -0500

    JwtFiler,java is modified

--- todo-list-api/src/main/java/com/todo/list/api/configuration/JwtFilter.java
+++ todo-list-api/src/main/java/com/todo/list/api/configuration/JwtFilter.java
@@ -20,7 +20,7 @@ import java.util.Collection;
 import java.util.Collections;
 
 @Component
-public class JwtFilter extends OncePerRequestFilter {
+public class            JwtFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtil jwtUtil;
