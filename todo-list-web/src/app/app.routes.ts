71b9d6dade5580f6372df7af6d93d592f92ba730
Anderson <jabetancourtb@hotmail.com>
2024-08-20 20:28:46 -0500

    app.module.ts and app.routes.ts are modified

--- todo-list-web/src/app/app.routes.ts
+++ todo-list-web/src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
-import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { ProfileShowComponent } from "./components/profile/show/profile-show.component";
+import { UpdatePasswordComponent } from "./components/profile/update-password/update-password.component";
+import { RecoveryPasswordComponent } from "./components/recovery-password/recovery-password.component";
+import { ConfirmSignUpComponent } from "./components/sign-up/confirm/confirm-sign-up.component";
+import { SignUpComponent } from "./components/sign-up/sign-up/sign-up.component";
 import { TaskListComponent } from "./components/task/list/task-list.component";
 import { AuthGuard } from "./guard/auth.guard";
 
 const APP_ROUTES: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'sign-up', component: SignUpComponent },
+    { path: 'confirm-sign-up/:verificationCode', component: ConfirmSignUpComponent },
+    { path: 'profile', component: ProfileShowComponent, canActivate: [AuthGuard] },
+    { path: 'update-password', component: UpdatePasswordComponent, canActivate: [AuthGuard] },
+    { path: 'recovery-password', component: RecoveryPasswordComponent },
+    { path: 'recovery-password/:verificationCode', component: RecoveryPasswordComponent },
     { path: 'tasks', component: TaskListComponent, canActivate: [AuthGuard] },
+    { path: '', component: TaskListComponent, canActivate: [AuthGuard] },
     { path: '**', pathMatch: 'full', redirectTo: 'login' }
 ];
 
