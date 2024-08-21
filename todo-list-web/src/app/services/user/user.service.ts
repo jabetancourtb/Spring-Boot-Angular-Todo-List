05d9fbe66d823c9c267b8311a974aa786775c125
Anderson <jabetancourtb@hotmail.com>
2024-08-20 20:26:13 -0500

    User service is modified

--- todo-list-web/src/app/services/user/user.service.ts
+++ todo-list-web/src/app/services/user/user.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { UserDTO } from "src/app/models/user/userDto.model";
 import { UserModel } from "src/app/models/user/user.model";
 import { environment } from "src/environments/environment";
 import { CookieService } from "ngx-cookie-service";
 import { JwtModel } from "src/app/models/jwt/jwt.model";
+import { HeadersService } from "../util/header.service";
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from "ngx-toastr";
 
 
 @Injectable({
@@ -14,12 +17,27 @@ import { JwtModel } from "src/app/models/jwt/jwt.model";
 
 export class UserService {
 
-    constructor(private http : HttpClient, private cookieService : CookieService) { }
+    constructor(private http : HttpClient
+        , private cookieService : CookieService
+        , private headersService : HeadersService
+        , private toastr: ToastrService) { }
 
-    login(email: string, password: string) : Promise<JwtModel> {
-        let apiUrl : string = environment.API_URL_DEVELOPMENT + "/user/" + email + "/" + password;
+        
+    signUp(userDto: UserDTO) : Observable<UserModel> {
+        let apiUrl : string = environment.API_URL_DEVELOPMENT + "/user/sign-up";
+        return this.http.post<UserModel>(apiUrl, userDto).pipe(
+            catchError(err => {
+                this.toastr.success('The email has been sent');
+                return throwError(err);
+            })
+        );
+    }
+
+    
+    updateUser(userDto: UserDTO) : Promise<JwtModel>{
+        let apiUrl : string = environment.API_URL_DEVELOPMENT + '/user';
         return new Promise((resolve, reject) => {
-            this.http.get<JwtModel>(apiUrl).subscribe(data => {
+            this.http.put<JwtModel>(apiUrl, userDto).subscribe(data => {
                     this.cookieService.set(environment.AUTHORIZATION_HEADER, data.jwt, { expires : 5/24 });
                     resolve(data);
                 }, 
@@ -30,12 +48,39 @@ export class UserService {
         });
     }
 
-    signUp(userDto: UserDTO) : Observable<UserModel> {
-        let apiUrl : string = environment.API_URL_DEVELOPMENT + "/user";
-        return this.http.post<UserModel>(apiUrl, userDto);
+    updatePassword(userId: number, currentPassword: string, newPassword: string) : Observable<any> {
+        let apiUrl : string = environment.API_URL_DEVELOPMENT + '/user/update-password/' + userId + "/" + currentPassword + "/" + newPassword;
+        return this.http.put<any>(apiUrl, null, this.headersService.authorizationHeader).pipe(catchError(err => {
+            return throwError(err);
+        }));
     }
 
-    logout(): void {
-        this.cookieService.deleteAll();
+    getUserByVerificationCode(verificationCode: string): Observable<UserModel> {
+        let apiUrl : string = environment.API_URL_DEVELOPMENT + '/user/get-user-by-verification-code/' + verificationCode;
+        return this.http.get<any>(apiUrl, this.headersService.authorizationHeader).pipe(catchError(err => {
+            return throwError(err);
+        }));
     }
+
+    getUserByEmail(email: string): Observable<UserModel> {
+        let apiUrl : string = environment.API_URL_DEVELOPMENT + '/user/get-user-by-email/' + email;
+        return this.http.get<any>(apiUrl, this.headersService.authorizationHeader).pipe(catchError(err => {
+            return throwError(err);
+        }));
+    }
+
+    updateVerificationCode(userId: number) : Observable<any> {
+        let apiUrl : string = environment.API_URL_DEVELOPMENT + '/user/update-verification-code/' + userId;
+        return this.http.put<any>(apiUrl, null, this.headersService.authorizationHeader).pipe(catchError(err => {
+            return throwError(err);
+        }));
+    }
+
+    updateState(userId: number, state: boolean) : Observable<any> {
+        let apiUrl : string = environment.API_URL_DEVELOPMENT + '/user/update-state/' + userId + "/" + state;
+        return this.http.put<any>(apiUrl, null, this.headersService.authorizationHeader).pipe(catchError(err => {
+            return throwError(err);
+        }));
+    }
+    
 }
